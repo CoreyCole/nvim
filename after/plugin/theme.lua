c4d670a252f002b53f6a26de5e5474252b63d18f
Corey Cole <coreyleoc@gmail.com>
2022-10-06 13:56:24 -0700

    feat: transparent background

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,5 +1,33 @@
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
-vim.opt.background = "dark" -- or "light" for light mode
+-- vim.g.tokyonight_transparent_sidebar = true
+-- vim.g.tokyonight_transparent = true
+-- vim.opt.background = "dark" -- or "light" for light mode
 -- vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[
+hi Normal guibg=none ctermbg=none
+hi NormalNC guibg=none ctermbg=none
+hi Comment guibg=none ctermbg=none
+hi Constant guibg=none ctermbg=none
+hi Special guibg=none ctermbg=none
+hi Identifier guibg=none ctermbg=none
+hi Statement guibg=none ctermbg=none
+hi PreProc guibg=none ctermbg=none
+hi Type guibg=none ctermbg=none
+hi Underlined guibg=none ctermbg=none
+hi Todo guibg=none ctermbg=none
+hi String guibg=none ctermbg=none
+hi Function guibg=none ctermbg=none
+hi Conditional guibg=none ctermbg=none
+hi Repeat guibg=none ctermbg=none
+hi Operator guibg=none ctermbg=none
+hi Structure guibg=none ctermbg=none
+hi LineNr guibg=none ctermbg=none
+hi Folded guibg=none ctermbg=none
+hi NonText guibg=none ctermbg=none
+hi SpecialKey guibg=none ctermbg=none
+hi VertSplit guibg=none ctermbg=none
+hi SignColumn guibg=none ctermbg=none
+hi EndOfBuffer guibg=none ctermbg=none
+hi CursorLineNr guibg=none ctermbg=none
+hi Keyword guibg=none ctermbg=none
+]])
 
--- lua/plugins/plugconf.lua
+++ lua/plugins/plugconf.lua
@@ -1,3 +1,28 @@
+-- vscode colorscheme
+local c = require("vscode.colors")
+require("vscode").setup({
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        vscLineNumber = "#A3A3A3",
+    },
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+    }
+})
+
 require("bufferline").setup({
     options = {
         buffer_close_icon = "",
@@ -5,13 +30,56 @@ require("bufferline").setup({
         -- indicator_icon = " ",
         left_trunc_marker = "",
         modified_icon = "●",
-        offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
+        offsets = { { filetype = "NvimTree", text = "SAUCE", text_align = "center" } },
         right_mouse_command = "Bdelete! %d",
         right_trunc_marker = "",
         show_close_icon = false,
         show_tab_indicators = true,
     },
     highlights = {
+        --[[ fill = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLineNC" },
+        }, ]]
+        fill = { bg = "NONE" },
+        background = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLine" },
+        },
+        buffer_visible = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+        },
+        buffer_selected = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+        },
+        separator = {
+            fg = { attribute = "bg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLine" },
+        },
+        separator_selected = {
+            fg = { attribute = "fg", highlight = "Special" },
+            bg = { attribute = "bg", highlight = "Normal" },
+        },
+        separator_visible = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLineNC" },
+        },
+        close_button = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "StatusLine" },
+        },
+        close_button_selected = {
+            fg = { attribute = "fg", highlight = "normal" },
+            bg = { attribute = "bg", highlight = "normal" },
+        },
+        close_button_visible = {
+            fg = { attribute = "fg", highlight = "normal" },
+            bg = { attribute = "bg", highlight = "normal" },
+        },
+    },
+    --[[ highlights = {
         fill = {
             fg = { attribute = "fg", highlight = "Normal" },
             bg = { attribute = "bg", highlight = "StatusLineNC" },
@@ -52,7 +120,7 @@ require("bufferline").setup({
             fg = { attribute = "fg", highlight = "normal" },
             bg = { attribute = "bg", highlight = "normal" },
         },
-    },
+    }, ]]
 })
 
 require("lualine").setup({
@@ -190,12 +258,11 @@ local rt = {
     },
     server = {
         settings = {
-            on_attach = function(client)
+            on_attach = function(_, bufnr)
                 -- Hover actions
                 vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
                 -- Code action groups
-                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-                require "illuminate".on_attach(client)
+                vim.keymap.set("n", "<leader>r", rt.code_action_group.code_action_group, { buffer = bufnr })
             end,
             ["rust-analyzer"] = {
                 assist = {
@@ -211,10 +278,10 @@ local rt = {
                     references = true,
                     methodReferences = true,
                 },
-                --[[ hoverActions = {
+                hoverActions = {
                     references = true,
                 },
-                inlayHints = {
+                --[[inlayHints = {
                     chainingHints = true,
                     maxLength = 40,
                     parameterHints = true,
@@ -342,31 +409,6 @@ require("indent_blankline").setup({
         "IndentBlanklineIndent2",
     },
     show_trailing_blankline_indent = true,
-})
-
--- vscode colorscheme
-local c = require("vscode.colors")
-require("vscode").setup({
-    -- Enable transparent background
-    transparent = false,
-
-    -- Enable italic comment
-    italic_comments = true,
-
-    -- Disable nvim-tree background color
-    disable_nvimtree_bg = true,
-
-    -- Override colors (see ./lua/vscode/colors.lua)
-    color_overrides = {
-        vscLineNumber = "#A3A3A3",
-    },
-
-    -- Override highlight groups (see ./lua/vscode/theme.lua)
-    group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-        -- use colors from this colorscheme by requiring vscode.colors!
-        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-    }
 })
 
 require('lspconfig').tsserver.setup {
